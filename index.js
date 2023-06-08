f7965518360a899926a2d5e64a2ca5d060f160e1
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2023-06-08 22:14:08 +0700

    create server

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.json({ message: "hello world" }));
+
+app.listen(4000, () => console.log("server running"));
