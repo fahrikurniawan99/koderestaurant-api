39ad184ad8bc83af01048d05cdd11003efecb100
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2023-06-08 22:18:32 +0700

    test product route

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.json({ message: "hello world" }));
+app.get("/products", (req, res) => res.json({ message: "pruducts route" }));
 
 app.listen(4000, () => console.log("server running"));
