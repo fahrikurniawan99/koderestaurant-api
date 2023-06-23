288c16ef69277c96d4490810494e403352ea1e3e
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2023-06-23 16:30:26 +0700

    fix cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("./app/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
