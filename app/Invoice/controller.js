09901462b76f5927dc7306599995d3ffc226a88a
fahrikurniawan99 <fahrikurniawan954@gmail.com>
2023-06-26 09:56:03 +0700

    add populate nested

--- app/Invoice/controller.js
+++ app/Invoice/controller.js
@@ -2,7 +2,15 @@ const Invoice = require("./model");
 
 const getInvoice = async (req, res, next) => {
   try {
-    const invoice = await Invoice.findById(req.params.id).populate("order");
+    const invoice = await Invoice.findById(req.params.id)
+      .populate("order")
+      .populate({
+        path: "items",
+        populate: {
+          path: "product",
+          model: "Product",
+        },
+      });
     return res.status(200).json({
       data: invoice,
     });
